b38bc2d216f10061232a0313743d09ba5623f30a
aschaves1976 <roots17@gmail.com>
2019-01-28 21:01:23 -0200

    Node - Arquivo package,json + Desafio

---
+++ funcionarios/funcionarios.js
@@ -0,0 +1,35 @@
+const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
+const axios = require('axios')
+
+const genero = g => g.genero === 'F'
+const pais = p => p.pais === 'China'
+const menorSalario = (func, funcAtual) =>{
+    return func.salario > funcAtual.salario ? func : funcAtual
+}
+   
+axios.get(url).then(response =>{
+    const funcionarios = response.data
+       
+    let salario = -1 //filtro[1].salario
+    function menor (elem, ind, arr){
+        if (salario <= 0){
+            salario = elem.salario
+        }
+         if (elem.salario < salario){
+             salario = elem.salario
+         }
+         return salario
+    }
+    const filtro = funcionarios.filter(genero).filter(pais).filter(menor).filter(s => s.salario === salario)
+ 
+    console.log(filtro)
+    console.log(filtro.length)
+
+    const func = funcionarios
+    .filter(pais)
+    .filter(genero)
+    .reduce(menorSalario)
+
+    console.log('A mulher chinesa com o maior salário é:')
+    console.log(func)
+})
