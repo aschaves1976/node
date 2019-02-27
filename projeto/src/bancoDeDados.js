ca826d1d7a576ab5304b30f61286564a20a054a7
aschaves1976 <roots17@gmail.com>
2019-02-26 21:57:07 -0300

    Node - Projeto:API com Express #02

---
+++ projeto/src/bancoDeDados.js
@@ -0,0 +1,26 @@
+const sequence = {
+    _id: 1,
+    get id() {return this._id++} //retorna o incremento do id
+}
+
+// constante produto que será um objeto e terá como chave o id do produto
+const produtos = {}
+
+function salvarProduto(produto){
+    if (!produto.id) produto.id = sequence.id
+    produtos[produto.id] = produto
+    return produto
+}
+
+// pegar o produto por ID
+// caso produto esteja vazio ou undeffined será retornado um objeto vazio
+function getProduto(id){
+    return produtos[id] ||{}
+}
+
+// retornar todos os produtos
+function getProdutos(){
+    return Object.values(produtos) // retornar apenas os valores
+}
+
+module.exports = {salvarProduto, getProduto, getProdutos}
--- projeto/src/servidor.js
+++ projeto/src/servidor.js
@@ -2,9 +2,25 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const bancoDeDados = require('./bancoDeDados')
 
 app.get('/produtos', (req, res, next) =>{
-    res.send({nome: 'Notebook', preço: 1234.98}) // este objeto sera convertido para JSON
+    res.send(bancoDeDados.getProdutos())
+})
+
+// o método abaixo possui o parametro :id
+app.get('/produtos/:id', (req, res, next) =>{
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+// método POST para submeter
+app.post('/produtos', (req, res, next) =>{
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.name,
+        preco: req.body.preco
+    })
+    // retornar o produto como resposta
+    res.send(produto) // O método SEND gera um JSON
 })
 
 app.listen(porta, () =>{
